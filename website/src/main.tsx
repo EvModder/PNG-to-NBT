baaa36ac11b3d3e00435b59e5427c2a38058c9f6
EvModder <evmodder@gmail.com>
2026-03-04 04:40:58 -0600

    Fix trailing slashes

--- website/src/main.tsx
+++ website/src/main.tsx
@@ -2,4 +2,17 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+function canonicalizeTrailingSlash(): boolean {
+  const { pathname, search, hash } = window.location;
+  const trimmedPath = pathname.replace(/\/+$/, "");
+  const canonicalPath = `${trimmedPath}/` || "/";
+  if (canonicalPath !== pathname) {
+    window.location.replace(`${canonicalPath}${search}${hash}`);
+    return true;
+  }
+  return false;
+}
+
+if (!canonicalizeTrailingSlash()) {
+  createRoot(document.getElementById("root")!).render(<App />);
+}
